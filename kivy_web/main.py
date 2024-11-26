28f46c0ddfa7903836f750f6fad1d73a2c984a84
WilsonAmorim <wilamsa@gmail.com>
2024-11-26 10:25:43 -0300

    Mudança no kivywem - 2

--- kivy_web/main.py
+++ kivy_web/main.py
@@ -1,6 +1,7 @@
 from  kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.window import Window
+import json
 
 Window.size = (400, 600)
 
@@ -9,6 +10,30 @@ class Manager(ScreenManager):
     pass
 
 class Menu(Screen):
+    def checkStatus(self):
+        try:
+            with open("status.json", "r") as fileJson:
+                contentFile = json.load(fileJson)
+        except:
+            with open("status.json", "w") as fileJson:
+                contentFile = {"Status": 1}
+                json.dump(contentFile, fileJson)
+            
+        if contentFile["Status"] == 1:
+            App.get_running_app().root.transition.direction = "left"
+            App.get_running_app().root.current = "beforParty"
+        
+
+class CreateParty(Screen):
+    pass
+
+class BeforOfCreateParty(Screen):
+    pass
+        
+class Screen1(Screen):
+    pass
+
+class Screen2(Screen):
     pass
 
 class MyApp(App):
